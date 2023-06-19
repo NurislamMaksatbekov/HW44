ee29589d03e6bc912e44c0f58f4264e8681281f8
Nurislam <maksatbekkov@icloud.com>
2023-06-20 02:16:27 +0600

    added new classes and some fields for classes book and employee

--- src/entity/Book.java
+++ src/entity/Book.java
@@ -1,5 +1,67 @@
 package entity;
 
 public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+    private String genre;
+    private String language;
+    private boolean status;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
